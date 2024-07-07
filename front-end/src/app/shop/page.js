3236482d27c8a8b3ee5e691c95a0199b0dca9836
Simriti <simritib1@gmail.com>
2024-07-07 07:34:17 +0000

    fixed fetching

--- front-end/src/app/shop/page.js
+++ front-end/src/app/shop/page.js
@@ -23,6 +23,219 @@ export default function Home() {
 
   const [products, setProducts] = useState([]);
 
+  // product data use for demo
+  const productDataByCat = {
+    blush: [
+      {
+        product_id: 1,
+        product_name: "Rare Beauty Soft Pinch Liquid Blush",
+        price: "39.00",
+        category: "blush",
+        src: "/products/1_product.webp",
+      },
+      {
+        product_id: 22,
+        product_name: "Careline Oil Control Blush-On",
+        price: "10.00",
+        category: "blush",
+        src: "/products/22_product.jpg",
+      },
+    ],
+    face_wash: [
+      {
+        product_id: 2,
+        product_name: "Youth to the People Superfood Cleanser",
+        price: "52.95",
+        category: "face_wash",
+        src: "/products/2_product.jpg",
+      },
+    ],
+    fragrance: [
+      {
+        product_id: 3,
+        product_name: "Sol De Janeiro Cheirosa 59 Perfume Mist",
+        price: "60.00",
+        category: "fragrance",
+        src: "/products/3_product.webp",
+      },
+    ],
+    primer: [
+      {
+        product_id: 4,
+        product_name: "Too Faced Hangover Replenishing Face Primer",
+        price: "57.04",
+        category: "primer",
+        src: "/products/4_product.webp",
+      },
+    ],
+    conditioner: [
+      {
+        product_id: 5,
+        product_name: "Amika The Kure Bond Repair Conditioner",
+        price: "38.03",
+        category: "conditioner",
+        src: "/products/5_product.webp",
+      },
+    ],
+    lip_tint: [
+      {
+        product_id: 6,
+        product_name: "Merrezca Glow Ink Color Lip Tint #01 - Wild Berry 5 G",
+        price: "15.88",
+        category: "lip_tint",
+        src: "/products/6_product.png",
+      },
+    ],
+    lip_gloss: [
+      {
+        product_id: 7,
+        product_name: "Charmiss Glitter Lip Gloss",
+        price: "11.04",
+        category: "lip_gloss",
+        src: "/products/7_product.webp",
+      },
+    ],
+    lip_serum: [
+      {
+        product_id: 8,
+        product_name: "Cathy Doll Cc Lip Serum",
+        price: "13.25",
+        category: "lip_serum",
+        src: "/products/8_product.webp",
+      },
+    ],
+    lip_balm: [
+      {
+        product_id: 9,
+        product_name: "Nivea Shine Lip Balm",
+        price: "5.00",
+        category: "lip_balm",
+        src: "/products/9_product.png",
+      },
+      {
+        product_id: 16,
+        product_name: "Laneige Lip Glowy Balm Berry",
+        price: "28.00",
+        category: "lip_balm",
+        src: "/products/16_product.jpg",
+      },
+    ],
+    powder: [
+      {
+        product_id: 10,
+        product_name: "Pond's Magic Powder BB Double UV Protectionm",
+        price: "2.95",
+        category: "powder",
+        src: "/products/10_product.jpg",
+      },
+    ],
+    foundation: [
+      {
+        product_id: 11,
+        product_name: "Estée Lauder Double Wear Stay-in-Place Makeup SPF 10",
+        price: "82.00",
+        category: "foundation",
+        src: "/products/11_product.png",
+      },
+      {
+        product_id: 14,
+        product_name:
+          "Charlotte Tilbury Charlotte's Beautiful Skin Foundation 10 Neutral",
+        price: "82.00",
+        category: "foundation",
+        src: "/products/14_product.webp",
+      },
+    ],
+    lip_mask: [
+      {
+        product_id: 12,
+        product_name: "LANEIGE Lip Sleeping Mask - Sweet Candy - 20gr",
+        price: "34.00",
+        category: "lip_mask",
+        src: "/products/12_product.webp",
+      },
+    ],
+    essence: [
+      {
+        product_id: 13,
+        product_name: "RHODE Glazing Milk",
+        price: "40.75",
+        category: "essence",
+        src: "/products/13_product.webp",
+      },
+    ],
+    concealer: [
+      {
+        product_id: 15,
+        product_name: "Elf Hydrating Camo Concealer",
+        price: "5.43",
+        category: "concealer",
+        src: "/products/15_product.webp",
+      },
+    ],
+    brow_pencil: [
+      {
+        product_id: 17,
+        product_name: "Hourglass Arch Brow Sculpting Pencil",
+        price: "50.25",
+        category: "brow_pencil",
+        src: "/products/17_product.webp",
+      },
+    ],
+    face_mist: [
+      {
+        product_id: 18,
+        product_name: "CAUDALIE BEAUTY ELIXIR",
+        price: "75.00",
+        category: "face_mist",
+        src: "/products/18_product.webp",
+      },
+    ],
+    moisturiser: [
+      {
+        product_id: 19,
+        product_name: "Charlotte Tilbury Magic Water Cream",
+        price: "176.00",
+        category: "moisturiser",
+        src: "/products/19_product.webp",
+      },
+      {
+        product_id: 21,
+        product_name:
+          "Maybelline New York Fit Me Fresh Tint Vitamin C Spf50 Mazenonline",
+        price: "19.10",
+        category: "moisturiser",
+        src: "/products/21_product.png",
+      },
+    ],
+    mascara: [
+      {
+        product_id: 20,
+        product_name: "Benefit Cosmetics Mascara",
+        price: "39.38",
+        category: "mascara",
+        src: "/products/20_product.jpg",
+      },
+    ],
+    setting_product: [
+      {
+        product_id: 23,
+        product_name:
+          "Dr Sensitive All Day No Sebum Blur Powder Translucent With Free Kabuki Brush",
+        price: "11.00",
+        category: "setting_product",
+        src: "/products/23_product.jpg",
+      },
+      {
+        product_id: 24,
+        product_name: "ZEESEA Lasting Makeup Setting Spray",
+        price: "39.38",
+        category: "setting_product",
+        src: "/products/24_product.webp",
+      },
+    ],
+  };
+
   // To input the search input on page load
   useEffect(() => {
     if (query) {
@@ -45,19 +258,16 @@ export default function Home() {
 
   // To fetch product data from MongoDB
   useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const response = await fetch(`/api/products?query=${query}`); // Need to change
-
-        const data = await response.json();
-        setProducts(data);
-      } catch (error) {
-        console.error("Error fetching products:", error);
+    const fetchProducts = () => {
+      if (prodCategory) {
+        const categoryKey = prodCategory.toLowerCase().replace(/\s+/g, "_");
+        const categoryProducts = productDataByCat[categoryKey] || [];
+        setProducts(categoryProducts);
       }
     };
 
     fetchProducts();
-  }, [query]);
+  }, [prodCategory]);
 
   return (
     <div className="flex min-h-screen flex-col bg-white">
@@ -211,10 +421,10 @@ export default function Home() {
           </div>
         </div>
 
-         {/* Product Grid */}
+        {/* Product Grid */}
         <div className="grid grid-cols-2 gap-4" style={{ marginTop: "-1vh" }}>
           {products.map((product) => (
-            <FeaturedProdCard key={product._id} product={product} />
+            <FeaturedProdCard key={product.product_id} product={product} />
           ))}
         </div>
       </main>
